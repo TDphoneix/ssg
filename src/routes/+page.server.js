0d662379bc964c79001e6be62d63be80e27af54e
borshon <borshon0101@gmail.com>
2024-12-21 10:57:21 +0600

    2

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,14 @@
 export function load() {
+
+    let all = 'gogo'
+
+    let time = new Date()
+
+    console.log('from back')
+
+
     return {
-        name:'joe'
+        name:'joe',
+        seconds:time.getSeconds()
     }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,5 @@
 <div class="flex justify-center mt-8">
     <h1 class="text-orange-500 text-2xl">hello world</h1>
     <p>my name is {data.name}</p>
+    <p>time is {data.seconds}</p>
 </div>
