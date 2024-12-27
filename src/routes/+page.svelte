d3e03b48e2914d3ff9c8ba8ea9fce5896aa09670
borshon <borshon0101@gmail.com>
2024-12-27 14:47:01 +0600

    modified root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     let {data} = $props()
 </script>
 
-<div class="flex justify-center mt-8">
+<div class="flex flex-col items-center gap-y-4 mt-8">
     <h1 class="text-orange-500 text-2xl">hello world</h1>
     <p>my name is {data.name}</p>
     <p>time is {data.seconds}</p>
